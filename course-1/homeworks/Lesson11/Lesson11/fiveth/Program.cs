ed20e29931a61c74c172dabf138fd95e1b24423d
Artem Stremilov <artemstremil@gmail.com>
2025-12-17 20:58:41 +0300

    feat:Доделаны 2 задачи из урока номер 11

---
+++ course-1/homeworks/Lesson11/Lesson11/fiveth/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lesson11.fiveth
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(int dx, int dy)
+        {
+            this.X += dx;
+            this.Y += dy;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Point({X}, {Y})");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Point(2, 3); 
+            var p2 = p1; 
+            p2.Move(5, 5); 
+            p1.Print();
+        }
+    }
+}
---
+++ course-1/homeworks/Lesson11/Lesson11/fourth/Program.cs
@@ -0,0 +1,60 @@
+﻿//using System;
+
+//namespace Lesson11.fourth
+//{
+//    class User
+//    {
+//        public string login {  get; set; }
+
+//        private string password;
+
+//        public User(string login, string password)
+//        {
+//            this.login = login;
+//            Password = password;
+//        }
+
+//        public string Password
+//        {
+//            get
+//            {
+//                return password;
+//            }
+//            set
+//            {
+//                if(value.Length > 0)
+//                {
+//                    password = value;
+//                    Console.WriteLine("Пороль обновлен");
+//                }
+//                else
+//                {
+//                    Console.WriteLine("Пароль не может быть пустым");
+//                }
+//            }
+//        }
+
+//        public void CheckPassword(string input)
+//        {
+//            if (input == this.Password)
+//            {
+//                Console.WriteLine("Доступ открыт");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Пароль неверный. Доступ ограничен");
+//            }
+//        }
+//    }
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            User user = new User("Sh@dow", "1234");
+//            user.CheckPassword("password");
+//            user.CheckPassword("1234");
+
+//        }
+//    }
+//}
