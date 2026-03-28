9ffbd961d7e8a156f19fc1b503d72890cefe074c
Artem Stremilov <artemstremil@gmail.com>
2026-03-28 12:30:01 +0300

    feat:добавлено дз номер 13

---
+++ course-1/homeworks/hw13/hw13/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw13
+{
+    namespace ChatBot.Settings
+    {
+        public static class ChatApiSettings
+        {
+            public static string BaseUrl { get; } = "https://openrouter.ai/api/v1/chat/completions";
+            public static string ApiKey { get; } = "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";
+            public static string DefaultModel { get; } = "google/gemma-3-1b-it:free";
+
+        }
+    }
+}
